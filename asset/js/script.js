00bff6f293eb5ec86fc74fc5eca0c274b7aa8d14
salonikahar <160547054+salonikahar@users.noreply.github.com>
2024-06-14 16:10:06 +0530

    Add files via upload

---
+++ asset/js/script.js
@@ -0,0 +1,39 @@
+let yourtext = [
+    ["Web Designer", "#2F4455"],
+    ["Full Stack Devloper", "#2F4455"],
+    ["Backend Devloper", "#2F4455"]
+  ];
+  
+  let x = 0;
+  let y = 0;
+  //how fast typing is
+  let wait = 100;
+  //how long you want to text stay before overwriting
+  let additionalwait = 5;
+  let txt = yourtext[0][0].split("");
+  let out = "";
+  let retyping = setInterval(function () {
+    document.getElementById("changingText").innerHTML = out;
+    if (x > txt.length - 1) {
+    } else {
+      out += txt[x];
+    }
+    x++;
+    if (x == txt.length + 2 + additionalwait) {
+      if (y == yourtext.length - 1) {
+        y = 0;
+        txt = yourtext[y][0].split("");
+        out = "";
+        document.getElementById("changingText").innerHTML = out;
+        document.getElementById("changingText").style.color = yourtext[y][1];
+        x = 0;
+      } else {
+        y += 1;
+        txt = yourtext[y][0].split("");
+        out = "";
+        document.getElementById("changingText").innerHTML = out;
+        document.getElementById("changingText").style.color = yourtext[y][1];
+        x = 0;
+      }
+    }
+  }, wait);
